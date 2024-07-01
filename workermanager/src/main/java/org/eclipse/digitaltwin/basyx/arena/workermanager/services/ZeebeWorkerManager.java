0b77a50c839d495337cbd3d2958c1d2d18433958
mateusmolina <mateus.molina@iese.fraunhofer.de>
2024-07-01 17:07:22 +0200

    Implement ZeebeWorkerManager

--- workermanager/src/main/java/org/eclipse/digitaltwin/basyx/arena/workermanager/services/ZeebeWorkerManager.java
+++ workermanager/src/main/java/org/eclipse/digitaltwin/basyx/arena/workermanager/services/ZeebeWorkerManager.java
@@ -2,11 +2,15 @@ package org.eclipse.digitaltwin.basyx.arena.workermanager.services;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.concurrent.CompletableFuture;
+import java.util.Map;
 
+import org.eclipse.digitaltwin.basyx.arena.workermanager.skills.Skill;
 import org.springframework.stereotype.Service;
 
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.client.api.worker.JobWorker;
 
 /**
  * 
@@ -18,17 +22,32 @@ public class ZeebeWorkerManager {
 
     private final ZeebeClient zeebeClient;
 
-    private final Deque<Long> workerIds = new ArrayDeque<>();
+    protected final Deque<JobWorker> workers = new ArrayDeque<>();
 
     public ZeebeWorkerManager(ZeebeClient zeebeClient) {
         this.zeebeClient = zeebeClient;
     }
 
-    public CompletableFuture<Long> deployWorker() {
-        throw new RuntimeException("todo");
+    public JobWorker deployWorker(Skill skill) {
+        JobWorker jobWorker = zeebeClient.newWorker().jobType(skill.skillId())
+                .handler((jobClient, job) -> handleJob(jobClient, job, skill)).open();
+
+        workers.add(jobWorker);
+
+        return jobWorker;
     }
 
-    public CompletableFuture<?> abortWorker(long workerId) {
-        throw new RuntimeException("todo");
+    public void abortWorker(JobWorker worker) {
+        if (!workers.remove(worker))
+            throw new RuntimeException("Worker " + worker + " isn't managed.");
+
+        worker.close();
     }
+
+    private static void handleJob(JobClient jobClient, ActivatedJob job, Skill skill) {
+        Map<String, Object> outputs = skill.skillFunction().apply(job.getVariablesAsMap());
+
+        jobClient.newCompleteCommand(job).variables(outputs).send().join();
+    }
+
 }
---
+++ workermanager/src/test/java/org/eclipse/digitaltwin/basyx/arena/workermanager/services/ZeebeWorkerManagerTest.java
@@ -0,0 +1,81 @@
+package org.eclipse.digitaltwin.basyx.arena.workermanager.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.eclipse.digitaltwin.basyx.arena.workermanager.skills.Skill;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.worker.JobWorker;
+import io.camunda.zeebe.process.test.extension.testcontainer.ZeebeProcessTest;
+
+@ZeebeProcessTest
+public class ZeebeWorkerManagerTest {
+
+        ZeebeClient zeebeClient;
+
+        private static final String JOB_ID = "test-skill";
+
+        @Test
+        void testDeployWorker() throws InterruptedException {
+
+                ZeebeWorkerManager workerManager = new ZeebeWorkerManager(zeebeClient);
+
+                TestListener testListener = Mockito.spy(new TestListener());
+
+                JobWorker worker = workerManager.deployWorker(buildTestSkill(JOB_ID, testListener));
+
+                assertThat(worker).isEqualTo(workerManager.workers.peek());
+
+                deployAndInstantiateTestBPMN();
+
+                Thread.sleep(1000);
+
+                verify(testListener, times(1)).call(any());
+        }
+
+        @Test
+        void testAbortWorker() throws InterruptedException {
+                ZeebeWorkerManager workerManager = new ZeebeWorkerManager(zeebeClient);
+
+                TestListener testListener = Mockito.spy(new TestListener());
+
+                JobWorker worker = workerManager.deployWorker(buildTestSkill(JOB_ID, testListener));
+
+                assertThat(worker).isEqualTo(workerManager.workers.peek());
+
+                deployAndInstantiateTestBPMN();
+
+                workerManager.abortWorker(worker);
+
+                assertThat(workerManager.workers).doesNotContain(worker);
+
+                deployAndInstantiateTestBPMN(); // shouldn't add +1 invocation
+
+                Thread.sleep(1000);
+
+                verify(testListener, times(1)).call(any());
+        }
+
+        private void deployAndInstantiateTestBPMN() {
+                zeebeClient.newDeployResourceCommand().addResourceFromClasspath("test-skill.bpmn").send()
+                                .thenCompose(p -> zeebeClient.newCreateInstanceCommand()
+                                                .bpmnProcessId(p.getProcesses().get(0).getBpmnProcessId())
+                                                .latestVersion().send())
+                                .toCompletableFuture().join();
+        }
+
+        static Skill buildTestSkill(String skillId, TestListener listener) {
+                return new Skill(skillId, listener::call);
+        }
+
+        private class TestListener {
+                public <T> T call(T in) {
+                        return in;
+                }
+        }
+}
